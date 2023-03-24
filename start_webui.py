a284dbba38fc5304db59d07c39c394483a756bb5
Nguyen Ngoc Toan <toan16072000@gmail.com>
2023-03-24 22:59:59 +0700

    Delete start_webui.py

--- start_webui.py
+++
@@ -1,3 +0,0 @@
-import os
-os.chdir("/content/stable-diffusion-webui")
-os.system("python launch.py --listen --xformers --enable-insecure-extension-access --theme dark --gradio-queue --multiple --ckpt-dir /content/drive/MyDrive/AI-Model-2022/ --no-half-vae --disable-nan-check")
