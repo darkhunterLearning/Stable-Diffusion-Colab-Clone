fd07c7e3d81921e07035c631059a5a86b241c595
Nguyen Ngoc Toan <toan16072000@gmail.com>
2023-05-04 15:04:34 +0700

    Update clone_from_gdrive.py

--- clone_from_gdrive.py
+++ clone_from_gdrive.py
@@ -6,7 +6,7 @@ linux_commands = [
   "cp -r /content/drive/MyDrive/SD_FILE/embedding/*.pt /content/stable-diffusion-webui/embeddings/",
   "cp -r /content/drive/MyDrive/SD_FILE/embedding/*.safetensors /content/stable-diffusion-webui/embeddings/",
   "cp -r /content/drive/MyDrive/SD_FILE/hypernet/*.pt /content/stable-diffusion-webui/models/hypernetworks/",
-  "cp -r /content/drive/MyDrive/SD_FILE/vae/*.pt /content/stable-diffusion-webui/models/VAE/",
+  "cp -r /content/drive/MyDrive/SD_FILE/vae/*.safetensors /content/stable-diffusion-webui/models/VAE/",
   "cp -r /content/drive/MyDrive/SD_FILE/vae/*.ckpt /content/stable-diffusion-webui/models/VAE/",
   "cp -r /content/drive/MyDrive/SD_FILE/lora/*.safetensors /content/stable-diffusion-webui/models/Lora/",
   "cp -r /content/drive/MyDrive/SD_FILE/embedding/*.png /content/stable-diffusion-webui/embeddings/",
