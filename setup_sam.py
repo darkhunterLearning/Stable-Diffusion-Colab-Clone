24664831a451f670747a162a69de56de77f797dc
Nguyen Ngoc Toan <toan16072000@gmail.com>
2023-05-16 21:15:18 +0700

    Update setup_sam.py

--- setup_sam.py
+++ setup_sam.py
@@ -1,7 +1,8 @@
 import os
 
 linux_commands = [
-        "git clone https://github.com/hnmr293/sd-webui-cutoff /content/stable-diffusion-webui/extensions/sd-webui-cutoff",
+        "git clone https://github.com/continue-revolution/sd-webui-segment-anything /content/stable-diffusion-webui/extensions/sd-webui-segment-anything",
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/Wang-Dong/SAM-vit-h/blob/main/sam_vit_h_4b8939.pth -d /content/stable-diffusion-webui/extensions/sd-webui-segment-anything/models/sam -o sam_vit_h_4b8939.pth"
 ]
 
 for command in linux_commands:
