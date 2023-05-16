af4d3034ee48023d00bdd2c0870e8cab94783369
Nguyen Ngoc Toan <toan16072000@gmail.com>
2023-05-16 21:48:49 +0700

    Update setup_sam.py

--- setup_sam.py
+++ setup_sam.py
@@ -2,7 +2,7 @@ import os
 
 linux_commands = [
         "git clone https://github.com/continue-revolution/sd-webui-segment-anything /content/stable-diffusion-webui/extensions/sd-webui-segment-anything",
-        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/Wang-Dong/SAM-vit-h/blob/main/sam_vit_h_4b8939.pth -d /content/stable-diffusion-webui/extensions/sd-webui-segment-anything/models/sam -o sam_vit_h_4b8939.pth"
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/Wang-Dong/SAM-vit-h/blob/main/sam_vit_h_4b8939.pth -d /content/stable-diffusion-webui/extensions/sd-webui-segment-anything/models/sam -o sam_vit_h_4b8939.pth\n"
 ]
 
 for command in linux_commands:
