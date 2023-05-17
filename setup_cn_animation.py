12eb45134261224f2ef8f5db8740d795ede2b56c
Nguyen Ngoc Toan <toan16072000@gmail.com>
2023-05-17 18:05:51 +0700

    Delete setup_cn_animation.py

--- setup_cn_animation.py
+++
@@ -1,8 +0,0 @@
-import os
-
-linux_commands = [
-        "git clone https://github.com/volotat/SD-CN-Animation /content/stable-diffusion-webui/extensions/SD-CN-Animation",
-]
-
-for command in linux_commands:
-  os.system(command)
