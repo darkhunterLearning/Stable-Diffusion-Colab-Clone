526b8c70dd06394e1431b0835f565c5d92032823
Nguyen Ngoc Toan <toan16072000@gmail.com>
2023-05-22 09:51:29 +0700

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import os 
 os.chdir("/content/stable-diffusion-webui/")
 linux_commands = [
-  "python launch.py --listen --xformers --enable-insecure-extension-access --theme dark --gradio-queue --multiple --ckpt-dir /content/drive/MyDrive/AI-Model-2022/ --no-half-vae --disable-nan-check"
+  "python launch.py --listen --xformers --enable-insecure-extension-access --theme dark --gradio-queue --multiple --ckpt-dir /content/drive/MyDrive/AI-Model-2022/ --no-half-vae --disable-nan-check --disable-safe-unpickle"
 ]
 
 for command in linux_commands:
