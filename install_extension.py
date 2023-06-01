6cd8555afb62c16bd5f419f966b969fb05a17d4d
Nguyen Ngoc Toan <toan16072000@gmail.com>
2023-06-01 09:37:20 +0700

    Update install_extension.py

--- install_extension.py
+++ install_extension.py
@@ -14,7 +14,8 @@ linux_commands = [
   "pip install -q torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1+cu118 torchtext==0.15.1 torchdata==0.6.0 --extra-index-url https://download.pytorch.org/whl/cu118 -U",
   "apt -y install -qq aria2 libcairo2-dev pkg-config python3-dev",
   "pip install -q xformers==0.0.19 triton==2.0.0 -U",
-  "git clone -b v2.2 https://github.com/camenduru/stable-diffusion-webui",
+  "git clone -b v2.3 https://github.com/camenduru/stable-diffusion-webui",
+  "git clone https://huggingface.co/embed/negative /content/stable-diffusion-webui/embeddings/negative",
   "wget https://raw.githubusercontent.com/camenduru/stable-diffusion-webui-scripts/main/run_n_times.py -O /content/stable-diffusion-webui/scripts/run_n_times.py",
   "git clone https://github.com/AlUlkesh/stable-diffusion-webui-images-browser /content/stable-diffusion-webui/extensions/stable-diffusion-webui-images-browser",
   "git clone https://github.com/camenduru/a1111-sd-webui-locon  /content/stable-diffusion-webui/extensions/a1111-sd-webui-locon",
@@ -27,8 +28,6 @@ linux_commands = [
   "git clone https://github.com/hnmr293/posex /content/stable-diffusion-webui/extensions/posex",
   "git clone https://github.com/camenduru/sd-webui-tunnels /content/stable-diffusion-webui/extensions/sd-webui-tunnels",
   "git clone https://github.com/etherealxx/batchlinks-webui /content/stable-diffusion-webui/extensions/batchlinks-webui",
-  "git clone https://github.com/hnmr293/sd-webui-cutoff /content/stable-diffusion-webui/extensions/sd-webui-cutoff",
-  "git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui-rembg /content/stable-diffusion-webui/extensions/stable-diffusion-webui-rembg",
   "git clone https://github.com/adieyal/sd-dynamic-prompts /content/stable-diffusion-webui/extensions/sd-dynamic-prompts",
   "git clone https://github.com/hako-mikan/sd-webui-regional-prompter /content/stable-diffusion-webui/extensions/sd-webui-regional-prompter",
   "git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111 /content/stable-diffusion-webui/extensions/ultimate-upscale-for-automatic1111"
